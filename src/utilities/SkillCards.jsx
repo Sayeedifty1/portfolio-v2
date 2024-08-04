2c0cf88011141f2f77cb23a6149a4508392eeb43
Sayeedifty1 <md.abusayeedifty@gmail.com>
2024-08-04 23:07:19 +0600

    Refactor SkillCards component styles

--- src/utilities/SkillCards.jsx
+++ src/utilities/SkillCards.jsx
@@ -9,29 +9,29 @@ const SkillCards = ({
   linkUrl,
 }) => {
   return (
-    <div className=" bg-gray-200 p-6 rounded-xl shadow-lg max-w-md mx-auto">
+    <div className=" hover:bg-gray-200 hover:text-gray-800  border hover:border-black p-6 rounded-xl shadow-lg max-w-md mx-auto">
       <div className="flex items-center justify-between mb-4">
-        <h2 className="text-2xl  text-gray-800 ">{title}</h2>
-        <Icon className="text-gray-800 w-6 h-6" />
+        <h2 className="text-2xl   ">{title}</h2>
+        <Icon className=" w-6 h-6" />
       </div>
       <div className="flex mb-4">
         <ul className="mr-4">
           {categories.left.map((item, index) => (
-            <li key={index} className="text-gray-800 text-left ">
+            <li key={index} className="text-left ">
               • {item}
             </li>
           ))}
         </ul>
         <ul>
           {categories.right.map((item, index) => (
-            <li key={index} className="text-gray-800 text-left ">
+            <li key={index} className="text-left ">
               • {item}
             </li>
           ))}
         </ul>
       </div>
-      <p className="text-gray-600 mb-4 text-left ">{description}</p>
-      <p href={linkUrl} className="text-gray-800  underline text-left ">
+      <p className="mb-4 text-left ">{description}</p>
+      <p href={linkUrl} className="underline text-left ">
         {linkText}
       </p>
     </div>
