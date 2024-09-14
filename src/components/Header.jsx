4918d094090a4feaf6bc1fd503e39a0f0c288ece
Sayeedifty1 <md.abusayeedifty@gmail.com>
2024-09-14 18:58:23 +0600

    new navbar addded and Update padding in about__wrapper

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,66 @@
 import React from "react";
 import { FaArrowRight, FaSun } from "react-icons/fa";
 
+import {
+  IconBrandGithub,
+  IconBrandX,
+  IconExchange,
+  IconHome,
+  IconNewSection,
+  IconTerminal2,
+} from "@tabler/icons-react";
+import { Navbar } from "./ui/navbar";
 const Header = () => {
+  const handleDownload = () => {
+    window.open("/Resume phero.pdf", "_blank");
+  };
+  const links = [
+    {
+      title: "Home",
+      icon: (
+        <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+
+    {
+      title: "Products",
+      icon: (
+        <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Components",
+      icon: (
+        <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+
+    {
+      title: "Changelog",
+      icon: (
+        <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+
+    {
+      title: "Twitter",
+      icon: (
+        <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "GitHub",
+      icon: (
+        <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+  ];
   return (
     <header className="bg-[#202020] text-white p-8">
       <div className="text-center">
@@ -17,20 +76,20 @@ const Header = () => {
           solutions .
         </p>
         <div className="mt-8 flex justify-center space-x-4">
-          <a
-            href="#notion-templates"
+          <button
+            onClick={handleDownload}
             className="border border-white text-white hover:text-gray-900 px-6 py-2 rounded-full flex items-center space-x-2 hover:bg-white transition"
           >
             <span>Download Resume</span> <FaArrowRight />
-          </a>
+          </button>
           <a
-            href="#learn-notion"
+            href="#contact"
             className="border border-white text-white hover:text-gray-900 px-6 py-2 rounded-full flex items-center space-x-2 hover:bg-white transition"
           >
             <span>Contact</span> <FaArrowRight />
           </a>
         </div>
-        <div className="flex justify-center mt-[50px]">
+        {/* <div className="flex justify-center mt-[50px]">
           <div className="w-[60%]">
             <div className="mt-8 flex justify-evenly border border-white rounded-full py-2 px-4 mx-auto">
               <a href="#home" className="text-white hover:text-gray-300">
@@ -50,6 +109,13 @@ const Header = () => {
               </div>
             </div>
           </div>
+        </div> */}
+        {/* navbar */}
+        <div className="flex items-center justify-center h-[12rem] w-full">
+          <Navbar
+            mobileClassName="translate-y-20" // only for demo, remove for production
+            items={links}
+          />
         </div>
       </div>
     </header>
